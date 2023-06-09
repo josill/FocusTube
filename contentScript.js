0add34b302bc32e77b23f5581bf8a151f43c0be5
Jonathan Sillak <113284402+josill@users.noreply.github.com>
2023-06-09 15:56:36 +0300

    Option to disable video previews, recommendations and comments

--- contentScript.js
+++ contentScript.js
@@ -2,31 +2,42 @@ if (typeof browser === "undefined") {
   browser = typeof chrome !== "undefined" ? chrome : null;
 }
 
-let showThumbnails = true;
-let customThumbnails = true;
-let customThumbnailsOption = "OPACITY";
-
 // Function to handle messages received from the background script
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.action === "URL_CHANGE") {
-    console.log(customThumbnailsOption)
-    if (customThumbnailsOption === "OPACITY") {
-      blurThumbnails();
-    } else if (customThumbnailsOption === "HIDE") {
-      hideThumbnails();
-    }
-  }
-  if (message.action == "OPACITY" && customThumbnails == true) {
-    // Handle the message action here
-    customThumbnailsOption = "OPACITY";
-  }
-  if (message.action == "HIDE" && customThumbnails == true) {
-    // Handle the message action here
-    customThumbnailsOption = "HIDE";
+    // Disabling thumbnails
+    chrome.storage.local.get(["customThumbnailsOption"]).then((result) => {
+      chrome.storage.local.get(["customThumbnails"]).then((customThumbnails) => {
+        if (customThumbnails["customThumbnails"]) {
+          if (result["customThumbnailsOption"] == "BLUR") {
+            blurThumbnails();
+          } else if (result["customThumbnailsOption"] == "HIDE") {
+            hideThumbnails();
+          }
+        }
+      });
+    });
+
+    // Disabling previews
+    chrome.storage.local.get(["customPreviews"]).then((result) => {
+      if (result["customPreviews"]) {
+        disableVideoPreview();
+      }
+    })
+
+    // Disabling recommendations when watching a video
+    chrome.storage.local.get(["customRecommendations"]).then((result) => {
+      console.log(result)
+      // TODO implement functions
+    })
+
+    // Disabling comments when watching a video
+    chrome.storage.local.get(["customComments"]).then((result) => {
+      console.log(result)
+      // TODO implement functions
+    })
   }
 });
-
-// TODO disable preview of videos
 
 // Function to change the opacity of thumbnails
 function blurThumbnails() {
@@ -37,6 +48,7 @@ function blurThumbnails() {
       filter: blur(12px) !important;
     }
   `;
+  // TODO blur channel images as well
   document.head.appendChild(style);
 }
 
@@ -45,6 +57,19 @@ function hideThumbnails() {
   const style = document.createElement("style");
   style.textContent = `
     yt-image {
+      display: none !important;
+    }
+  `;
+  // TODO blur channel images as well
+  document.head.appendChild(style);
+}
+
+function disableVideoPreview() {
+  // TODO disable previews on creators channels.
+
+  const style = document.createElement("style");
+  style.textContent = `
+    ytd-video-preview {
       display: none !important;
     }
   `;
--- popup.js
+++ popup.js
@@ -1,28 +1,134 @@
 // popup.js
 
-// Function to send a message to the content script
-function sendMessageToContentScript(action) {
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      const activeTab = tabs[0];
-      console.log(activeTab.url);
-    sendMessage(activeTab.id, { action });
-    });
+const thumbnailsToggle = document.getElementById("thumbnailsToggle");
+const blackBtn = document.getElementById("blackBtn");
+const blurBtn = document.getElementById("blurBtn");
+const previewsToggle = document.getElementById("previewsToggle");
+const recommendationsToggle = document.getElementById("recommendationsToggle");
+const commentsToggle = document.getElementById("commentsToggle");
+
+window.onload = function() {
+  chrome.storage.local.get(["customThumbnails"]).then((result) => {
+    let customThumbnails = result["customThumbnails"];
+    let thumbnailTogglerContainer = document.getElementsByClassName("thumbnails-hide-options-toggle");
+
+    if (customThumbnails) {
+      thumbnailsToggle.checked = true;
+      blurBtn.checked = true;
+
+      Array.from(thumbnailTogglerContainer).forEach(element => {
+        element.style.display = "flex";
+      });
+    } 
+  });
+
+  chrome.storage.local.get(["customThumbnailsOption"]).then((result) => {
+    let customThumbnailsOption = result["customThumbnailsOption"];
+
+    if (customThumbnailsOption == "BLUR") {
+      blurBtn.checked = true;
+      blackBtn.checked = false;
+    } else if (customThumbnailsOption == "HIDE") {
+      blurBtn.checked = false;
+      blackBtn.checked = true;
+    }
+  });
+
+  chrome.storage.local.get(["customPreviews"]).then((result) => {
+    if (result["customPreviews"]) {
+      previewsToggle.checked = true;
+    }
+  })
+
+  chrome.storage.local.get(["customRecommendations"]).then((result) => {
+    if (result["customRecommendations"]) {
+      recommendationsToggle.checked = true;
+    }
+  });
+
+  chrome.storage.local.get(["customComments"]).then((result) => {
+    if (result["customComments"]) {
+      commentsToggle.checked = true;
+    }
+  });
 }
 
 function sendMessage(tabId, message) {
   chrome.tabs.sendMessage(tabId, message);
 }
 
-// Add click event listener for the Opacity button
-const opacityBtn = document.getElementById("opacityBtn");
-opacityBtn.addEventListener("click", () => {
-  console.log("opacitybtn")
-    sendMessageToContentScript("OPACITY");
-});
+// Function to send a message to the content script
+function sendMessageToContentScript(action) {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      const activeTab = tabs[0];
+    sendMessage(activeTab.id, { action });
+    });
+}
 
-// Add click event listener for the Black button
-const blackBtn = document.getElementById("blackBtn");
-blackBtn.addEventListener("click", () => {
-    console.log("blackbtn");
-    sendMessageToContentScript("HIDE");
-});
+function handleClick(event) {
+  if (event.target.id == "thumbnailsToggle") {
+    if (thumbnailsToggle.checked == true) {
+      chrome.storage.local.set({"customThumbnails": true});
+    } else {
+      chrome.storage.local.set({"customThumbnails": false});
+    }
+    toggleThumbnails()
+    return;
+    // sendMessageToContentScript()
+  } else if (event.target.id == "blurBtn") {
+    chrome.storage.local.set({"customThumbnailsOption": "BLUR"});
+    // sendMessageToContentScript("BLUR");
+    blurBtn.checked = true;
+    blackBtn.checked = false;
+    return;
+  } else if (event.target.id == "blackBtn") {
+    chrome.storage.local.set({"customThumbnailsOption": "HIDE"});
+    // sendMessageToContentScript("HIDE");
+    blurBtn.checked = false;
+    blackBtn.checked = true;
+    return;
+  } else if (event.target.id == "previewsToggle") {
+    if (previewsToggle.checked == true) {
+        chrome.storage.local.set({ "customPreviews": true });
+    } else {
+        chrome.storage.local.set({ "customPreviews": false });
+    }
+    return;
+  } else if (event.target.id == "recommendationsToggle") {
+    if (recommendationsToggle.checked == true) {
+      chrome.storage.local.set({ "customRecommendations": true });
+    } else {
+      chrome.storage.local.set({ "customRecommendations": false });
+    }    return;
+  } else if (event.target.id == "commentsToggle") {
+    if (commentsToggle.checked == true) {
+      chrome.storage.local.set({ "customComments": true });
+    } else {
+      chrome.storage.local.set({ "customComments": false });
+    }
+    return;
+  }
+}
+
+function toggleThumbnails() {
+    let customThumbnails;
+    chrome.storage.local.get(["customThumbnails"]).then((result) => {
+      customThumbnails = result["customThumbnails"];
+      let thumbnailTogglerContainer = document.getElementsByClassName(
+        "thumbnails-hide-options-toggle"
+      );
+
+      if (customThumbnails) {
+        Array.from(thumbnailTogglerContainer).forEach((element) => {
+          element.style.display = "flex";
+        });
+      } else {
+        Array.from(thumbnailTogglerContainer).forEach((element) => {
+          element.style.display = "none";
+        });
+      }
+    });
+}
+
+
+document.addEventListener("click", handleClick);

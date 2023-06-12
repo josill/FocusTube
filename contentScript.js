1243ca5f51d2e9c85577382475e31a0df616f8af
goot666 <jonathan.sillak2@gmail.com>
2023-06-12 23:47:36 +0300

    disable recommendations, comments and main page siderbar. Popout has three modes: light, focus and custom.

--- contentScript.js
+++ contentScript.js
@@ -2,48 +2,91 @@ if (typeof browser === "undefined") {
   browser = typeof chrome !== "undefined" ? chrome : null;
 }
 
-// Function to handle messages received from the background script
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.action === "URL_CHANGE") {
+function handleLoad() {
+  // console.log(window.location.href);
+  chrome.storage.local.get().then((settings) => {
     // Disabling thumbnails
-    chrome.storage.local.get(["customThumbnailsOption"]).then((result) => {
-      chrome.storage.local.get(["customThumbnails"]).then((customThumbnails) => {
-        if (customThumbnails["customThumbnails"]) {
-          if (result["customThumbnailsOption"] == "BLUR") {
-            blurThumbnails();
-          } else if (result["customThumbnailsOption"] == "HIDE") {
-            hideThumbnails();
-          }
-        }
-      });
-    });
+    if (settings["customThumbnailsOption"] == "BLUR") {
+      blurThumbnails();
+    } else if (settings["customThumbnailsOption"] == "HIDE") {
+      hideThumbnails();
+    }
 
     // Disabling previews
-    chrome.storage.local.get(["customPreviews"]).then((result) => {
-      if (result["customPreviews"]) {
-        disableVideoPreview();
-      }
-    })
+    if (settings["customPreviews"]) {
+      disableVideoPreview();
+    }
 
-    // Disabling recommendations when watching a video
-    chrome.storage.local.get(["customRecommendations"]).then((result) => {
-      console.log(result)
-      // TODO implement functions
-    })
+    if (settings["customRecommendations"]) {
+      disableRecommendations();
+    }
 
     // Disabling comments when watching a video
-    chrome.storage.local.get(["customComments"]).then((result) => {
-      console.log(result)
-      // TODO implement functions
-    })
-  }
+    if (settings["customComments"]) {
+      disableComments();
+    }
+
+    // Disabling the sidebar when on the home page
+    if (settings["customSidebar"]) {
+      disableSidebar();
+    }
+
+    observeElements();
+  });
+}
+
+document.addEventListener("DOMContentLoaded", handleLoad);
+// implement url change
+window.addEventListener("popstate", handleLoad);
+document.addEventListener("popstate", function () {
+  console.log("location changed!");
 });
+
+// function handleMutation(mutationsList, observer) {
+//   // still appears when first come to yt and click on a vid
+//   for (let mutation of mutationsList) {
+//     if (mutation.type == "childList" && mutation.addedNodes.length > 0) {
+//       let element = document.getElementById("secondary");
+//       if (element) {
+//         chrome.storage.local.get(["customRecommendations"]).then((setting) => {
+//           if (setting["customRecommendations"]) {
+//             disableRecommendations();
+//           }
+//         });
+//         observer.disconnect();
+//         break;
+//       }
+//     }
+//   }
+// }
+
+function observeElements() {
+  const observer = new MutationObserver(function (mutationsList) {
+    mutationsList.forEach(function (mutation) {
+      if (mutation.type === "childList" && mutation.addedNodes.length > 0) {
+        mutation.addedNodes.forEach(function (node) {
+          console.log(node);
+          // Perform specific actions on the added node if needed
+        });
+        disableVideoPreview();
+        observer.disconnect();
+      }
+    });
+  });
+
+  observer.observe(document.body, { childList: true, subtree: true });
+}
 
 // Function to change the opacity of thumbnails
 function blurThumbnails() {
   const style = document.createElement("style");
   style.textContent = `
     yt-image {
+      opacity: 0.4 !important;
+      filter: blur(12px) !important;
+    }
+
+    ytd-thumbnail {
       opacity: 0.4 !important;
       filter: blur(12px) !important;
     }
@@ -66,10 +109,111 @@ function hideThumbnails() {
 
 function disableVideoPreview() {
   // TODO disable previews on creators channels.
-
   const style = document.createElement("style");
+  const videoPreviews = document.getElementsByTagName("ytd-compact-video-renderer");
+  console.log(videoPreviews)
+  Array.from(videoPreviews).forEach((p) => {
+    console.log(p.classList.get('hovered'))
+    console.log(p.classList)
+    p.classList.remove('hovered');
+  })
   style.textContent = `
     ytd-video-preview {
+      display: none !important;
+    }
+
+    .mouseover-overlay {
+      display: none !important;
+    }
+  `;
+  document.head.appendChild(style);
+}
+
+function disableRecommendations() {
+
+  const style = document.createElement("style");
+  // TODO video end is fucked
+
+  style.textContent = `
+    #secondary {
+      display: none !important;
+    }
+
+    #related {
+      display: none !important;
+    }
+
+    .html5-video-container {
+      width: 100% !important;
+      height: 100% !important;
+    }
+
+    video {
+      width: 100% !important;
+      height: 100% !important;
+    }
+
+    .ytp-chrome-bottom {
+      width: 98% !important;
+    }
+
+    .ytp-progress-bar-container {
+      width: 100% !important;
+    }
+
+    .ytp-chrome-controls {
+      width: 100% !important;
+    }
+    
+    .ytp-progress-list {
+      width: 100% !important;
+    }
+
+    .ytp-chapter-hover-container {
+      width: 100% !important;
+    }
+
+    .ytp-progress-bar-padding {
+      display: none !important;
+    }
+
+    .ytp-heat-map-container {
+      width: 100% !important;
+    }
+
+    .ytp-clip-end-exclude {
+      left: 150% !important;
+    }
+  `;
+  document.head.appendChild(style);
+}
+
+function disableComments() {
+  const style = document.createElement("style");
+  style.textContent = `
+    ytd-comments {
+      display: none !important;
+    }
+  `;
+  document.head.appendChild(style);
+}
+
+function disableSidebar() {
+  const style = document.createElement("style");
+  style.textContent = `
+    ytd-guide-section-renderer {
+      display: none !important;  
+    }
+
+    ytd-guide-renderer {
+      display: none !important;
+    }
+
+    ytd-mini-guide-renderer {
+      display: none !important;
+    }
+
+    yt-icon-button {
       display: none !important;
     }
   `;
--- popup.js
+++ popup.js
@@ -1,29 +1,33 @@
 // popup.js
 
+const contentDiv = document.getElementsByClassName("options-container");
 const thumbnailsToggle = document.getElementById("thumbnailsToggle");
 const blackBtn = document.getElementById("blackBtn");
 const blurBtn = document.getElementById("blurBtn");
 const previewsToggle = document.getElementById("previewsToggle");
 const recommendationsToggle = document.getElementById("recommendationsToggle");
 const commentsToggle = document.getElementById("commentsToggle");
+const modeTogglers = document.getElementsByClassName("mode-toggle")
+const lightModeToggle = document.getElementById("light-mode-toggle")
+const focusModeToggle = document.getElementById("focus-mode-toggle")
+const customModeToggle = document.getElementById("custom-mode-toggle")
 
 window.onload = function() {
-  chrome.storage.local.get(["customThumbnails"]).then((result) => {
-    let customThumbnails = result["customThumbnails"];
-    let thumbnailTogglerContainer = document.getElementsByClassName("thumbnails-hide-options-toggle");
+  chrome.storage.local.get().then((settings) => {
+    let customThumbnails = settings["customThumbnails"];
+    let thumbnailTogglerContainer = document.getElementsByClassName(
+      "thumbnails-hide-options-toggle"
+    );
 
     if (customThumbnails) {
       thumbnailsToggle.checked = true;
       blurBtn.checked = true;
 
-      Array.from(thumbnailTogglerContainer).forEach(element => {
+      Array.from(thumbnailTogglerContainer).forEach((element) => {
         element.style.display = "flex";
       });
-    } 
-  });
-
-  chrome.storage.local.get(["customThumbnailsOption"]).then((result) => {
-    let customThumbnailsOption = result["customThumbnailsOption"];
+    }
+    let customThumbnailsOption = settings["customThumbnailsOption"];
 
     if (customThumbnailsOption == "BLUR") {
       blurBtn.checked = true;
@@ -32,79 +36,86 @@ window.onload = function() {
       blurBtn.checked = false;
       blackBtn.checked = true;
     }
-  });
 
-  chrome.storage.local.get(["customPreviews"]).then((result) => {
-    if (result["customPreviews"]) {
+    if (settings["customPreviews"]) {
       previewsToggle.checked = true;
     }
-  })
 
-  chrome.storage.local.get(["customRecommendations"]).then((result) => {
-    if (result["customRecommendations"]) {
+    if (settings["customRecommendations"]) {
       recommendationsToggle.checked = true;
     }
-  });
 
-  chrome.storage.local.get(["customComments"]).then((result) => {
-    if (result["customComments"]) {
+    if (settings["customComments"]) {
       commentsToggle.checked = true;
+    }
+
+    if (settings["customSidebar"]) {
+      sidebarToggle.checked = true;
     }
   });
 }
 
-function sendMessage(tabId, message) {
-  chrome.tabs.sendMessage(tabId, message);
-}
+// function sendMessage(tabId, message) {
+//   chrome.tabs.sendMessage(tabId, message);
+// }
 
-// Function to send a message to the content script
-function sendMessageToContentScript(action) {
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      const activeTab = tabs[0];
-    sendMessage(activeTab.id, { action });
-    });
-}
+// // Function to send a message to the content script
+// function sendMessageToContentScript(action) {
+//   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+//       const activeTab = tabs[0];
+//     sendMessage(activeTab.id, { action });
+//     });
+// }
 
 function handleClick(event) {
   if (event.target.id == "thumbnailsToggle") {
     if (thumbnailsToggle.checked == true) {
-      chrome.storage.local.set({"customThumbnails": true});
+      chrome.storage.local.set({ customThumbnails: true });
     } else {
-      chrome.storage.local.set({"customThumbnails": false});
+      chrome.storage.local.set({ customThumbnails: false });
     }
-    toggleThumbnails()
+    toggleThumbnails();
     return;
     // sendMessageToContentScript()
   } else if (event.target.id == "blurBtn") {
-    chrome.storage.local.set({"customThumbnailsOption": "BLUR"});
+    chrome.storage.local.set({ customThumbnailsOption: "BLUR" });
     // sendMessageToContentScript("BLUR");
     blurBtn.checked = true;
     blackBtn.checked = false;
     return;
   } else if (event.target.id == "blackBtn") {
-    chrome.storage.local.set({"customThumbnailsOption": "HIDE"});
+    chrome.storage.local.set({ customThumbnailsOption: "HIDE" });
     // sendMessageToContentScript("HIDE");
     blurBtn.checked = false;
     blackBtn.checked = true;
     return;
   } else if (event.target.id == "previewsToggle") {
     if (previewsToggle.checked == true) {
-        chrome.storage.local.set({ "customPreviews": true });
+      chrome.storage.local.set({ customPreviews: true });
     } else {
-        chrome.storage.local.set({ "customPreviews": false });
+      chrome.storage.local.set({ customPreviews: false });
     }
     return;
   } else if (event.target.id == "recommendationsToggle") {
     if (recommendationsToggle.checked == true) {
-      chrome.storage.local.set({ "customRecommendations": true });
+      chrome.storage.local.set({ customRecommendations: true });
     } else {
-      chrome.storage.local.set({ "customRecommendations": false });
-    }    return;
+      chrome.storage.local.set({ customRecommendations: false });
+    }
+    return;
   } else if (event.target.id == "commentsToggle") {
     if (commentsToggle.checked == true) {
-      chrome.storage.local.set({ "customComments": true });
+      chrome.storage.local.set({ customComments: true });
     } else {
-      chrome.storage.local.set({ "customComments": false });
+      chrome.storage.local.set({ customComments: false });
+    }
+    return;
+  } else if (event.target.id == "sidebarToggle") {
+    console.log(sidebarToggle.checked);
+    if (sidebarToggle.checked == true) {
+      chrome.storage.local.set({ customSidebar: true });
+    } else {
+      chrome.storage.local.set({ customSidebar: false });
     }
     return;
   }
@@ -119,6 +130,9 @@ function toggleThumbnails() {
       );
 
       if (customThumbnails) {
+        if (blackBtn.checked == false && blurBtn.checked == false) {
+          blurBtn.checked = true;
+        }
         Array.from(thumbnailTogglerContainer).forEach((element) => {
           element.style.display = "flex";
         });
@@ -130,5 +144,44 @@ function toggleThumbnails() {
     });
 }
 
+function handleModeToggle(event) {
+  var selectedMode = event.target.id;
 
-document.addEventListener("click", handleClick);
+  Array.from(modeTogglers).forEach(function (button) {
+    button.classList.remove("active");
+  });
+  event.target.classList.add("active");
+
+  console.log(selectedMode);
+  console.log(contentDiv);
+
+  // Update the content based on the selected mode
+  if (selectedMode === "light-mode-toggle") {
+    contentDiv.innerHTML = `
+        <h1>Welcome to Light Mode!</h1>
+        <p>This is the content for Light Mode.</p>
+      `;
+  } else if (selectedMode === "dark-mode-toggle") {
+    contentDiv.innerHTML = `
+        <h1>Welcome to Dark Mode!</h1>
+        <p>This is the content for Dark Mode.</p>
+      `;
+  } else if (selectedMode === "custom-mode-toggle") {
+    contentDiv.innerHTML = `
+        <h1>Welcome to Custom Mode!</h1>
+        <p>This is the content for Custom Mode.</p>
+      `;
+  }
+}
+
+// Toggle between modes
+document.addEventListener("DOMContentLoaded", function () {
+  var activeButton = document.getElementById("light-mode-toggle");
+  activeButton.classList.add("active");
+
+  document.addEventListener("click", handleClick);
+
+  Array.from(modeTogglers).forEach(function (button) {
+    button.addEventListener("click", handleModeToggle);
+  });
+});
